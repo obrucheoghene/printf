d0c5ad2d0bd0f65eb49907a404e73903575b1024
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-24 10:08:32 -0800

    remove print newline

--- printf.c
+++ printf.c
@@ -39,7 +39,7 @@ int _printf(const char *format, ...)
 		format++;
 	}
 
-	putchar('\n');
+	va_end(list);
 	return (len);
 
 }
--- test/main.c
+++ test/main.c
@@ -8,11 +8,8 @@
 int main(void)
 {
 	int len = 0;
-	char *s = "-596943495";
 
-	len = _printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+	len += _printf("Let's print a simple sentence.\n");
 
-
-_printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
 	return (len);
 }
